cef252abb4e995802ae0e8363b3203cc13afe975
rahull0328 <rahull0328@users.noreply.github.com>
2025-01-08 15:59:29 +0530

    Initial Commit

---
+++ base/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import uuid
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_add=True)
+    
+    class Meta:
+        abstract = True
