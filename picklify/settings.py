320aa2ce210231322aa3e4280c3708fc7ab763ed
rahull0328 <rahull0328@users.noreply.github.com>
2025-01-09 18:49:58 +0530

    added UI elements

--- picklify/settings.py
+++ picklify/settings.py
@@ -117,9 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# Default primary key field type
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles' )
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR, 'public/static')    
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_URL = '/media/'
+
+#Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
