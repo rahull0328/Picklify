5bec0325db9f9d09fa50599e2073bc217c8efef6
rahull0328 <rmmehta2004@gmail.com>
2025-02-12 21:41:45 +0530

    completed email sending on registration

--- base/emails.py
+++ base/emails.py
@@ -4,6 +4,6 @@ from django.core.mail import send_mail
 
 def sendAccountActivationEmail(email, emailToken):
     subject = 'Account Activation Email !'
-    emailFrom = settings.Email_MOST_USER
+    emailFrom = settings.EMAIL_HOST_USER
     message = f'Hi, Click on the below link to activate your account http://127.0.0.1:8000/accounts/activate/{emailToken}'
     send_mail(subject, message, emailFrom, [email])
