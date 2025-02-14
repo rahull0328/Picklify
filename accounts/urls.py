120d4f0fcc77c2886eedaefa23b1fd6eaa3d970f
rahull0328 <rmmehta2004@gmail.com>
2025-02-14 15:34:37 +0530

    starting again from scratch

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from accounts.views import loginPage, registerPage, activateEmail
-
-urlpatterns = [
-    path('login/', loginPage, name="login"),
-    path('register/', registerPage, name="register"),
-    path('activate/<emailToken>', activateEmail, name="activatEmail")
-]
