0b516d6f6da494771742c8be961e056b5a666cdf
rahull0328 <rmmehta2004@gmail.com>
2025-02-25 13:31:31 +0530

    added model for size and colour variant of products

--- products/models.py
+++ products/models.py
@@ -12,16 +12,22 @@ class Category(BaseModel):
         self.slug = slugify(self.category_name)
         super(Category, self).save(*args, **kwargs)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.category_name
     
 class ColorVariant(BaseModel):
     color_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
     
+    def __str__(self) -> str:
+        return self.color_name
+    
 class SizeVariant(BaseModel):
     size_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
+    
+    def __str__(self) -> str:
+        return self.size_name
 
 class Product(BaseModel):
     product_name = models.CharField(max_length=100)
@@ -29,8 +35,8 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.IntegerField()
     product_description = models.TextField()
-    color_variant = models.ManyToManyField(ColorVariant)
-    size_variant = models.ManyToManyField(SizeVariant)
+    color_variant = models.ManyToManyField(ColorVariant, blank=True)
+    size_variant = models.ManyToManyField(SizeVariant, blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.product_name)
