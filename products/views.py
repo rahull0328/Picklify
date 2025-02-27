f5f38dc052be775a3ca774f4085d012e40a8aa61
rahull0328 <rmmehta2004@gmail.com>
2025-02-27 13:49:35 +0530

    fixed rendering of products as per their size

--- products/views.py
+++ products/views.py
@@ -9,18 +9,18 @@ def get_random_products():
 def get_product(request, slug):
     try:
         product = Product.objects.get(slug=slug)
-        context = {'product': product}
         random_products = Product.objects.exclude(slug=slug).order_by('?')[:3]
         
+        context = {'product': product, 'random_products': random_products}
+
         if request.GET.get('size'):
-            size = request.GET.get('size')
-            price = product.get_product_size_by_size(size)
+            size = request.GET.get('size').strip('"')
+            price = product.get_product_size_by_size(size)  # Ensure this method exists in your model
             context['selected_size'] = size
             context['updated_price'] = price
             print(price)
-            
-        return render(request, 'product/product.html', 
-                    context={'product': product, 'random_products': random_products})
+        
+        return render(request, 'product/product.html', context)
     
     except Product.DoesNotExist:
         return HttpResponseNotFound("Product not found")
