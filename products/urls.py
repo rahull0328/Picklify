c079ecbc0307dd06b0a5f7b1ed68bf9e3b0c996c
rahull0328 <rmmehta2004@gmail.com>
2025-04-08 22:51:39 +0530

    UI advancements with category filtering

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from products.views import get_product, view_categories, category_products
+from products.views import get_product, product_list_or_category
 
 urlpatterns = [
-   path('category/<slug:slug>/', category_products, name='category_products'),
-   path('categories/', view_categories, name='view_categories'),
-   path('<slug>/', get_product, name='get_product'),
+    # View all products or filtered by category
+    path('', product_list_or_category, name='product_list'),
+    path('products/category/<slug:slug>/', product_list_or_category, name='category_products'),
+
+    # Product detail page
+    path('<slug>/', get_product, name='get_product'),
 ]
